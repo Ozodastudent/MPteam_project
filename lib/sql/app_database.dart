6ec58b8d846d9338da3a22a7dcb70ac2d6688520
NigoraQodirova <98759486+NigoraQodirova@users.noreply.github.com>
2024-01-17 09:59:13 +0500

    Create app_database.dart
    Nigora is working on sql entitiy file

---
+++ lib/sql/app_database.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+import 'package:floor/floor.dart';
+import 'package:pinterestmobile/sql/data_access_object/images_list_dao.dart';
+import 'package:pinterestmobile/sql/entity/images_list_entity.dart';
+import 'package:sqflite/sqflite.dart' as sqflite;
+part 'app_database.g.dart';
+
+@Database(version: 1, entities: [ImagesListEntity])
+abstract class AppDatabase extends FloorDatabase {
+  ImagesListDao get imagesList;
+}
