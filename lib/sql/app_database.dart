8f56aec2b135ca68f3bac37bcb0bd92ff997162c
NigoraQodirova <98759486+NigoraQodirova@users.noreply.github.com>
2024-01-22 17:16:13 +0500

    Update app_database.dart
    Nigora changes some codes

--- lib/sql/app_database.dart
+++ lib/sql/app_database.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'package:floor/floor.dart';
-import 'package:pinterestmobile/sql/data_access_object/images_list_dao.dart';
-import 'package:pinterestmobile/sql/entity/images_list_entity.dart';
+import 'package:MPteam_project/sql/data_access_object/images_list_dao.dart';
+import 'package:MPteam_project/sql/entity/images_list_entity.dart';
 import 'package:sqflite/sqflite.dart' as sqflite;
 part 'app_database.g.dart';
 
